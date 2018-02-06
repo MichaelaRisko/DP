1722a28c682d9ceb3c09f28f0d981b105888fc2e
MichaelaRisko <30437749+MichaelaRisko@users.noreply.github.com>
2018-02-06 05:21:51 +0100

    roca program changed
    added right number of bits for a and k

--- programs/5_roca/roca.c
+++ programs/5_roca/roca.c
@@ -3,12 +3,15 @@
  * Department of Electronics and Multimedia Telecommunications
  *
  * Masters Thesis
- * Prime Number Generation for Embedded Cryptographic Applications
+ * Generating Prime Numbers for Embedded Cryptographic Applications
  * 
  * Student: Bc. Michaela Risko
  * Supervisor: doc. Ing. Milos Drutarovsky, PhD.
  *
- * REV 2.0 @ 30.01.2018
+ * REV 2.1 @ 30.01.2018
+ * 		--> params loading from input file
+ *		--> writing output to output file
+ *		--> fixed bugs
  */
 
 #include <stdio.h>
@@ -32,16 +35,12 @@ const char* IN_FILE_PATH = "files/in_file.txt";
 const int INT_BOUND_LOW = 992;
 const int INT_BOUND_HIGH = 1952;
 
-
 const int SEED = 32563;
-const int SIZE_A = 62; //bits
-const int SIZE_K = 37; //bits
+const int SIZE_A = 46; //bits
+const int SIZE_K = 53; //bits
 
-//void getRandomFromInterval() {
-//	int r = RAND_MAX;
-//	r 
-//	printf("%d", r);
-//}
+//The number of numbers generated
+int numGenerations = 0;
 
 BIGNUM* calculateM(int n) {
 	BN_CTX* ctx = BN_CTX_new();
@@ -51,21 +50,12 @@ BIGNUM* calculateM(int n) {
 	BN_one(product);
 	BNEASY_add(product, 1, FALSE);
 	
-	printf("product: ");
-	BNUTIL_cPrintln(product);
-	
 	int i;
-	for(i = 0; i < n; i++) {
-		printf("\n");
-		printf("i = %d\n", i);
+	for(i = 0; i < n - 1; i++) {
 		nextPrime = BNEASY_findNextPrime(nextPrime, FALSE, TRUE);
-		printf("nextPrime: ");
-		BNUTIL_cPrintln(nextPrime);
 		int success = BN_mul(product, product, nextPrime, ctx);
 		BNUTIL_successCheck(success, "calculateM", "Error multiplying "
 								"two BIGNUM* instances");
-		printf("product: ");
-		BNUTIL_cPrintln(product);
 	}
 	BN_CTX_free(ctx);
 	BN_free(nextPrime);
@@ -77,33 +67,26 @@ BIGNUM* calculateP(BIGNUM* a, BIGNUM* k, BIGNUM* m) {
 	
 	BN_CTX* ctx = BN_CTX_new();
 	
-	//calculate 65537^a --> res
+	//calculate 65537^a mod M --> mdop
 	BIGNUM* base = BN_new();
 	BN_set_word(base, 65537);
-	BIGNUM* res = BN_new();
-	
-	BN_exp(res, base, a, ctx);
-	BN_free(base);
-	
-	//calculate (res mod M) --> modop
-	BIGNUM* modop = NULL;
-	BN_mod(modop, res, m, ctx);
+	BIGNUM* modop = BN_new();
+	BN_mod_exp(modop, base, a, m, ctx);
 	
 	//calculate (k * M) --> mulop
-	BIGNUM* mulop = NULL;
+	BIGNUM* mulop = BN_new();
 	BN_mul(mulop, k, m, ctx);
 	
 	//calculate addition of mulop and modop
-	BIGNUM* result = NULL;
+	BIGNUM* result = BN_new();
 	BN_add(result, mulop, modop);
 	
 	BN_free(base);
+	BN_free(modop);
 	BN_free(mulop);
-	BN_free(res);
 	BN_CTX_free(ctx);
 	
 	return result;
-	return NULL;
 }
 
 BIGNUM* generatePrimeRoca() {
@@ -112,15 +95,14 @@ BIGNUM* generatePrimeRoca() {
 	BIGNUM* m;
 	BIGNUM* p;
 	
+	int n = 126;
+	m = calculateM(n);
+	
 	start:
 	a = BNEASY_generateRandomBN(SIZE_A);
-	//BNUTIL_cPrintln(a);
 	k = BNEASY_generateRandomBN(SIZE_K);
-	//BNUTIL_cPrintln(k);
-	int n = 39;
-	
-	m = calculateM(n);
 	p = calculateP(a, k, m);
+	numGenerations++;
 	
 	if(!BNEASY_isPrime(p)) {
 		goto start;
@@ -134,11 +116,62 @@ BIGNUM* generatePrimeRoca() {
 	return p;
 }
 
+/*
+ * This procedure writes the program result to a file @ filePath
+ *
+ * @param filePath
+ *			the pointer to a string containing the file path to the file where
+ *			text is to be written
+ * @param duration
+ *			the program execution time to be written to a file
+ * @param numGens
+ *			the number of times a number was generated using calculateP(...)
+ * @param primeNums
+ *			the number of prime numbers that were to be found
+ * @param numBits
+ *			the number of bits in the generated number
+ *
+ */
+void writeResultToFile(const char* filePath, float duration,
+								int numGen, int primeNums, int numBits) {
+	printf("writing result to file...\n");
+	char timestamp[20];
+	BNUTIL_setTimestampNow(timestamp);
+	FILEOPS_appendToFile(filePath, timestamp);
+	
+	char dur[100];
+	char text[] = " Found %d prime numbers (%d bit) in %.3f "
+						"seconds requiring %d number generations\n";
+	int charsWritten = snprintf(dur, 100, text, primeNums, numBits,
+	duration, numGen);
+	if(charsWritten < 0) {
+		BNUTIL_successCheck(FALSE, "writeResultToFile", "Error "
+								"executing snprintf");
+	}
+	FILEOPS_appendToFile(filePath, dur);
+}
+
 int main() {
 	printf("Program started...\n");
+	printf("Running Roca 1.0\n");
+	printf("Running program for generating 1024 bit primes...\n");
 	
-	BIGNUM* myPrime = generatePrimeRoca();
-	BNUTIL_cPrintln(myPrime);
+	printf("...reading params from input file...\n");
+	char bnGenCount_str[16];
+	FILEOPS_loadParamFromFile(IN_FILE_PATH, "bnGenCount", bnGenCount_str);
+	int bnGenCount = atoi(bnGenCount_str);
+	printf("...all params from input file read successfully...\n");
+	
+	printf("...starting algorithm...");
+	
+	clock_t start = clock();
+	int i;
+	for(i = 0; i < bnGenCount; i++) {
+		generatePrimeRoca();
+	}
+	clock_t end = clock();
+	float duration = (float)(end - start) / CLOCKS_PER_SEC;
+	writeResultToFile("files/out_file.txt", duration, numGenerations, bnGenCount, 1024);
 	
 	printf("Program terminated with success...");
 	return EXIT_SUCCESS;
